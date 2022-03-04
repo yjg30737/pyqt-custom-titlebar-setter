357fcb6d01a13cdacc3489d48604be867ef91d05
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-04 13:50:49 +0900

    Refactoring

--- pyqt_custom_titlebar_setter/customTitlebarSetter.py
+++ pyqt_custom_titlebar_setter/customTitlebarSetter.py
@@ -8,7 +8,6 @@ class CustomTitlebarSetter:
     @staticmethod
     def getCustomTitleBar(main_window: QWidget, icon_filename: str) -> CustomTitlebarWindow:
         titleBarWindow = CustomTitlebarWindow(main_window)
-        caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
-        titleBarWindow.setTopTitleBar(icon_filename=os.path.join(caller_path, icon_filename))
+        titleBarWindow.setTopTitleBar(icon_filename=icon_filename)
         titleBarWindow.setButtons()
         return titleBarWindow
