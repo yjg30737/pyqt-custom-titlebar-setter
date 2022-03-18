2789f3842ad1cc643bed38e97328d03548eec0d2
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-18 10:13:22 +0900

    Add arguments to getCustomTitleBar(style, hint)

--- pyqt_custom_titlebar_setter/customTitlebarSetter.py
+++ pyqt_custom_titlebar_setter/customTitlebarSetter.py
@@ -1,11 +1,15 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget
 from pyqt_custom_titlebar_window import CustomTitlebarWindow
 
 
 class CustomTitlebarSetter:
     @staticmethod
-    def getCustomTitleBar(main_window: QWidget, icon_filename: str) -> CustomTitlebarWindow:
+    def getCustomTitleBar(main_window: QWidget, icon_filename: str, style='windows',
+                          hint=Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint) -> CustomTitlebarWindow:
         titleBarWindow = CustomTitlebarWindow(main_window)
         titleBarWindow.setTopTitleBar(icon_filename=icon_filename)
+        titleBarWindow.setButtonStyle(style)
+        titleBarWindow.setButtonHint(hint)
         titleBarWindow.setButtons()
         return titleBarWindow
