580ed43fd01111c4dc0290aa6d8f31aca629f618
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-24 07:48:57 +0900

    Set icon_filename empty text by default

--- pyqt_custom_titlebar_setter/customTitlebarSetter.py
+++ pyqt_custom_titlebar_setter/customTitlebarSetter.py
@@ -5,7 +5,7 @@ from pyqt_custom_titlebar_window import CustomTitlebarWindow
 
 class CustomTitlebarSetter:
     @staticmethod
-    def getCustomTitleBar(main_window: QWidget, icon_filename: str, style='windows',
+    def getCustomTitleBar(main_window: QWidget, icon_filename: str = '', style='windows',
                           hint=Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint) -> CustomTitlebarWindow:
         titleBarWindow = CustomTitlebarWindow(main_window)
         titleBarWindow.setTopTitleBar(icon_filename=icon_filename)
