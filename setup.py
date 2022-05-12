203e119cf13acb66cdbf9818048f142ed5ce91cc
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 14:18:46 +0900

    Update url of package, update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-custom-titlebar-setter',
-    version='0.2.1',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Custom Titlebar Setter',
+    description='PyQt custom titlebar setter (movable/resizable, etc.)',
     url='https://github.com/yjg30737/pyqt-custom-titlebar-setter.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.15',
-        'pyqt-custom-titlebar-window @ git+https://git@github.com/yjg30737/pyqt-custom-titlebar-window.git@main'
+        'pyqt-custom-titlebar-window>=0.0.1'
     ]
 )
