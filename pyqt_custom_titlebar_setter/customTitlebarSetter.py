f8f3c96d637954bfea22f07f9430de0b7d100c19
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-16 17:40:58 +0900

    Remove 'style' argument

--- pyqt_custom_titlebar_setter/customTitlebarSetter.py
+++ pyqt_custom_titlebar_setter/customTitlebarSetter.py
@@ -4,11 +4,10 @@ from pyqt_custom_titlebar_window import CustomTitlebarWindow
 
 class CustomTitlebarSetter:
     @staticmethod
-    def getCustomTitleBarWindow(main_window: QWidget, icon_filename: str = '', style='windows',
+    def getCustomTitleBarWindow(main_window: QWidget, icon_filename: str = '',
                                 hint: list = ['min', 'max', 'close']) -> CustomTitlebarWindow:
         titleBarWindow = CustomTitlebarWindow(main_window)
         titleBarWindow.setTopTitleBar(icon_filename=icon_filename)
-        titleBarWindow.setButtonStyle(style)
         titleBarWindow.setButtonHint(hint)
         titleBarWindow.setButtons()
         return titleBarWindow
