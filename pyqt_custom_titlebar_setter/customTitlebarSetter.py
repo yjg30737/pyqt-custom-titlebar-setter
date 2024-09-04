0e79ae60e7cccf3a9e2d1a874b0eb7fb67de2c7a
Jung Gyu Yoon <yjg30737@gmail.com>
2024-09-04 17:06:21 +0900

    Add test.py and icon.svg to make it easy to test, Refactor import statement for CustomTitlebarWindow , update req.txt to install Dev branch of the pyqt-custom-titlebar-window for testing before deployment

--- pyqt_custom_titlebar_setter/customTitlebarSetter.py
+++ pyqt_custom_titlebar_setter/customTitlebarSetter.py
@@ -1,13 +1,13 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
-from pyqt_custom_titlebar_window import CustomTitlebarWindow
+from pyqt_custom_titlebar_window.customTitlebarWindow import CustomTitlebarWindow
 
 
 class CustomTitlebarSetter:
     @staticmethod
     def getCustomTitleBarWindow(main_window: QWidget, title: str = '', icon_filename: str = '',
-                                font: QFont = QFont('Arial', 14), hint: list = ['min', 'max', 'close'],
+                                font: QFont = QFont('Arial', 17), hint: list = ['min', 'max', 'close'],
                                 align=Qt.AlignCenter, bottom_separator: bool = False) -> CustomTitlebarWindow:
         titleBarWindow = CustomTitlebarWindow(main_window)
         titleBarWindow.setTopTitleBar(title=title, icon_filename=icon_filename, font=font, align=align,
